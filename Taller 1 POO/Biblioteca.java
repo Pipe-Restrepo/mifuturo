378d0073b262abca49ae9d5dac164f050ae66951
Juan Felipe Restrepo <frestrepos.jfrs@gmail.com>
2024-10-27 12:49:38 -0500

    Commit taller 3

--- Taller 1 POO/Biblioteca.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.interfaces;
-
-import com.biblioteca.modelos.Libro;
-import com.biblioteca.modelos.Usuario;
-
-public interface Biblioteca {
-        void prestarLibro(Libro libro, Usuario usuario);
-        void devolverLibro(Libro libro, Usuario usuario);
-}
